a546291adc3a9f73d5bc5ff40d8c02ff5ac3ed11
TomLERO <tomlr.pro@gmail.com>
2020-12-09 14:10:19 +0100

    src

---
+++ src/pattern/BarChartObserver.java
@@ -0,0 +1,53 @@
+package pattern;
+
+import java.awt.Color;
+
+import java.awt.Graphics;
+
+
+import utils.CourseRecord;
+import utils.LayoutConstants;
+
+/**
+ * This class represents a bar chart view of a vector of data. Uses the Observer
+ * pattern.
+ */
+@SuppressWarnings("serial")
+public class BarChartObserver extends ChartObserver {
+	public BarChartObserver(CourseData data) {
+		super(data);
+	}
+
+	/**
+	 * Paint method
+	 * 
+	 * @param g
+	 *            a Graphics object on which to paint
+	 */
+	public void paint(Graphics g) {
+		super.paint(g);
+		LayoutConstants.paintBarChartOutline(g, this.courseData.size());
+		for (int i = 0; i < courseData.size(); i++) {
+			CourseRecord record = (CourseRecord) courseData.elementAt(i);
+			g.setColor(Color.blue);
+			g.fillRect(
+					LayoutConstants.xOffset + (i + 1)
+							* LayoutConstants.barSpacing + i
+							* LayoutConstants.barWidth, LayoutConstants.yOffset
+							+ LayoutConstants.graphHeight
+							- LayoutConstants.barHeight
+							+ 2
+							* (LayoutConstants.maxValue - record
+									.getNumOfStudents()),
+					LayoutConstants.barWidth, 2 * record.getNumOfStudents());
+			g.setColor(Color.red);
+			g.drawString(record.getName(),
+					LayoutConstants.xOffset + (i + 1)
+							* LayoutConstants.barSpacing + i
+							* LayoutConstants.barWidth, LayoutConstants.yOffset
+							+ LayoutConstants.graphHeight + 20);
+		}
+	}
+
+	
+}
